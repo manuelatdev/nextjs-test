df28399f98c743ecca17894f38b4515d35b7f38a
Manuel <manuel.at.dev@gmail.com>
2024-04-03 18:42:12 +0200

    base

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './ui/global.css'
+import {inter} from './ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antianliased`}>
+        {children}</body>
+    </html>
+  );
+}
